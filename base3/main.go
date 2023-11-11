788165755987dce8ed5e9ae89e8ef37b85e2a07a
Renewdxin <cissyrenxin@icloud.com>
2023-11-11 19:47:50 +0800

    11.11 实现错误处理机制

--- base3/main.go
+++ base3/main.go
@@ -1,28 +1,20 @@
 package main
 
 import (
-	"gee"
 	"net/http"
+
+	"gee"
 )
 
 func main() {
 	r := gee.New()
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
-
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
